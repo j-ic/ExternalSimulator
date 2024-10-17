0b692189a74d21cd4e45946d190eaf75093d0866
김경은 <105626545+uvc-vivian@users.noreply.github.com>
2024-10-17 16:17:58 +0900

    🔧Facility DTO 업데이트(JsonPropertyName 추가)

--- ETLSimulator/DTO/Facility.cs
+++ ETLSimulator/DTO/Facility.cs
@@ -1,13 +1,23 @@
-﻿namespace ETLSimulator.DTO;
+﻿using System.Text.Json.Serialization;
+
+namespace ETLSimulator.DTO;
 
 public readonly record struct Facility
 {
+    [JsonPropertyName("LINE_ID")]
     public string LineId { get; init; }
+    [JsonPropertyName("TEMPERATURE")]
     public double Temperature { get; init; }
+    [JsonPropertyName("HUMIDITY")]
     public double Humidity { get; init; }
+    [JsonPropertyName("LINE_SPEED")]
     public double LineSpeed { get; init; }
+    [JsonPropertyName("UTILIZATION_RATE")]
     // 설비 가동률
     public double UtilizationRate { get; init; }
+    [JsonPropertyName("PRODUCTIVITY")]
     // 설비 생산성
     public double Productivity { get; init; }
+    [JsonPropertyName("TIMESTAMP")]
+    public DateTime Timestamp { get; init; }
 }
