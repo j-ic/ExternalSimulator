2a98980871071919d4b54d460accf67da9989aa8
김경은 <105626545+uvc-vivian@users.noreply.github.com>
2024-10-17 16:09:14 +0900

    ⚡Facility DTO 생성

---
+++ ETLSimulator/DTO/FacilityData.cs
@@ -0,0 +1,13 @@
+﻿namespace ETLSimulator.DTO;
+
+public readonly record struct FacilityData
+{
+    public string LineId { get; init; }
+    public double Temperature { get; init; }
+    public double Humidity { get; init; }
+    public double LineSpeed { get; init; }
+    // 설비 가동률
+    public double UtilizationRate { get; init; }
+    // 설비 생산성
+    public double Productivity { get; init; }
+}
