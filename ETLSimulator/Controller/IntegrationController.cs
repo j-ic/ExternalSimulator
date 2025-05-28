04bbad69323a1a6568cb9da63083eaa3ea55c7b3
주일찬 <jic@uvc.co.kr>
2025-05-28 17:05:37 +0900

    📟 사소한수정

--- ETLSimulator/Controller/IntegrationController.cs
+++ ETLSimulator/Controller/IntegrationController.cs
@@ -10,25 +10,35 @@ public class IntegrationController
 {
     #region Constructor
 
+    /// <summary>
+    /// Initializes a managed MQTT client and event handlers
+    /// </summary>
     public IntegrationController(IManagedMqttClient managedMqttClient)
     {
         _managedMqttClient = managedMqttClient;
+
+        // Event handler for connection state changed
         _managedMqttClient.ConnectionStateChangedAsync += async args =>
         {
             Console.WriteLine("Integration Client Connection state changed");
             await Task.CompletedTask;
         };
+
+        // Event handler for successful connection
         _managedMqttClient.ConnectedAsync += async args =>
         {
             Console.WriteLine($"Integration Client Connected: {args.ConnectResult.ResultCode}");
             await Task.CompletedTask;
         };
+
+        // Event handler for disconnection
         _managedMqttClient.DisconnectedAsync += async args =>
         {
             Console.WriteLine($"Integration Client Disconnected: {args.Reason}");
             await Task.CompletedTask;
         };
 
+        // Timer for printing message count
         _timer = new System.Timers.Timer(TimeSpan.FromMinutes(1));
         _timer.Elapsed += PrintMessageCount;
         _timer.Start();
@@ -36,43 +46,78 @@ public class IntegrationController
 
     #endregion
 
-    public async Task SendAGVLoop(string topic, string jobId, uint milliseconds, int maxCount)
+    /// <summary>
+    /// Send AGV data to MQTT topic in a loop
+    /// </summary>
+    /// <param name="topic"> MQTT topic to publish AGV data </param>
+    /// <param name="milliseconds"> Delay in milliseconds between each message </param>
+    /// <param name="maxCount"> Maximum number of AGV data to send </param>
+    public async Task SendAGVLoop(string topic, uint milliseconds, int maxCount)
     {
+        // Convert milliseconds to int
         int millisecondInt = (int)milliseconds;
+
+        // Loop to send AGV data
         while (true)
         {
+            // Create AGV list
             Dictionary<string, List<AGV>> agvList
                 = await Task.Run(() => CreateAGVList(maxCount));
+
+            // Serialize AGV list to JSON string
             string agvPayload = JsonSerializer.Serialize(agvList);
+
+            // Publish AGV data to MQTT
             await PublishMqtt(topic, agvPayload);
+
+            // Increment message count
             Interlocked.Increment(ref _messageCount);
 
+            // Delay for specified milliseconds
             await Task.Delay(millisecondInt);
         }
     }
 
-    public async Task SendTransportLoop(string topic, string jobId, uint milliseconds, int maxCount)
+    /// <summary>
+    /// Send transport data to MQTT topic in a loop
+    /// </summary>
+    /// <param name="topic"> MQTT topic to publish transport data </param>
+    /// <param name="milliseconds"> Delay in milliseconds between each message </param>
+    /// <param name="maxCount"> Maximum number of transport data to send </param>
+    public async Task SendTransportLoop(string topic, uint milliseconds, int maxCount)
     {
+        // Convert milliseconds to int
         int millisecondInt = (int)milliseconds;
+
+        // Loop to send transport data
         while (true)
         {
+            // Create transport list
             Dictionary<string, List<Transport>> transportList
                 = await Task.Run(() => CreateTransportList(maxCount));
 
+            // Serialize transport list to JSON string
             string payloadString = JsonSerializer.Serialize(transportList);
+
+            // Publish transport data to MQTT
             await PublishMqtt(topic, payloadString);
+
+            // Increment message count
             Interlocked.Increment(ref _messageCount);
 
+            // Delay for specified milliseconds
             await Task.Delay(millisecondInt);
         }
     }
 
     #region Private Methods
 
-    private Dictionary<string, List<Transport>> CreateTransportList(int count)
+    private static Dictionary<string, List<Transport>> CreateTransportList(int count)
     {
+        // Create transport list
         List<Transport> transportList = [];
 
+        // Main carrier ID array
         string[] mainCarrIdArray = [
             "2F38678", "2F49727", "3F84077", "3F16198", "2F27631", "3F48241",
             "3F40768", "2F13414", "3F54193", "2F49674", "3F00689", "3F11802",
@@ -80,6 +125,8 @@ public class IntegrationController
             "2F19687", "2F18741", "2F62227", "2F39188", "2F20734", "2F79661",
             "2F90372", "3F79847", "2F08989", "2F38216", "3F62528", "3F90339"
         ];
+
+        // Equipment ID array
         string[] eqpIdArray = [
             "HFB09ICS0600", "HFF09AGN0200", "HFF09AGN0200", "HFF11AGN0400", "HFF11CNV0500",
             "HFF09AGM0100", "HFF09AGN0200", "HFF11AGC0100", "HFF11AGN0100", "HFF09AGM0200",
@@ -92,16 +139,29 @@ public class IntegrationController
             "HFF09AGN0500", "HFF09ICS0400", "HFF11AGN0600", "HFF11CNV0300", "HFF11AGN0300",
             "HFF09AGC0500", "HFF11AGC0100", "HFB09ICS0500", "HFF09AGN0200", "HFF09AGN0600"
         ];
+
+        // System name array
         string[] sysNameArray = ["TC", "ADS", "MCS",];
+
+        // Move status array
         string[] moveStsArray = ["MOVING", "COMPLETE", "RECEIVE"];
 
+        // Loop to create transport data
         for (int i = 0; i < count; i++)
         {
+            // Randomly select main carrier ID
             string mainCarrId = mainCarrIdArray[Random.Shared.Next(0, mainCarrIdArray.Length - 1)];
+
+            // Randomly select equipment ID
             string fromEqpId = eqpIdArray[Random.Shared.Next(0, eqpIdArray.Length - 1)];
+
+            // Randomly select equipment ID
             string toEqpId = eqpIdArray[Random.Shared.Next(0, eqpIdArray.Length - 1)];
+
+            // Randomly select equipment ID
             string currEqpId = eqpIdArray[Random.Shared.Next(0, eqpIdArray.Length - 1)];
 
+            // Create transport data
             var transportDto = new Transport
             {
                 JobId = mainCarrId + "_" + Random.Shared.Next(0, 1000000).ToString(),
@@ -125,23 +185,36 @@ public class IntegrationController
                 UpdateUserId = sysNameArray[Random.Shared.Next(0, sysNameArray.Length - 1)]
             };
 
+            // Add transport data to list
             transportList.Add(transportDto);
         }
 
-        Dictionary<string, List<Transport>> dict = new();
-        dict["TRANSPORT_JOB"] = transportList;
+        // Create dictionary with transport list
+        Dictionary<string, List<Transport>> dict = new() 
+        {
+            ["TRANSPORT_JOB"] = transportList
+        };
 
         return dict;
     }
 
-    private Dictionary<string, List<AGV>> CreateAGVList(int count)
+    /// <summary>
+    /// Create AGV list with random data
+    /// </summary>
+    /// <param name="count"> Size of AGV list </param>
+    private static Dictionary<string, List<AGV>> CreateAGVList(int count)
     {
+        // Create AGV list
         List<AGV> agvList = [];
+
+        // AGV name array
         string[] vhlName = ["AGV_001", "AGV_002", "AGV_003", "AGV_004",
                             "AGV_005", "AGV_006", "AGV_007", "AGV_008"];
 
+        // Loop to create AGV data
         for (int i = 0; i < count; i++)
         {
+            // Create AGV data
             var agvDto = new AGV
             {
                 VhlName = vhlName[Random.Shared.Next(0, vhlName.Length - 1)],
@@ -155,15 +228,19 @@ public class IntegrationController
                 Degree = Random.Shared.Next(0, 100).ToString(),
             };
 
+            // Add AGV data to list
             agvList.Add(agvDto);
         }
 
-        Dictionary<string, List<AGV>> dict = new();
-        dict["AGV"] = agvList;
+        // Create dictionary with AGV list
+        Dictionary<string, List<AGV>> dict = new() { ["AGV"] = agvList };
 
         return dict;
     }
 
+    /// <summary>
+    /// publish MQTT message
+    /// </summary>
     private async Task PublishMqtt(string topic, string payloadString)
     {
         MqttApplicationMessage message = new MqttApplicationMessageBuilder()
@@ -175,11 +252,16 @@ public class IntegrationController
         await _managedMqttClient.EnqueueAsync(message);
     }
 
+    /// <summary>
+    /// Callback function that prints the total number of messages published
+    /// </summary>
     private void PrintMessageCount(object? sender, ElapsedEventArgs e)
     {
+        // Print message count
         string timestamp = DateTime.Now.ToString("hh:mm:ss.fff");
         Console.WriteLine($"[{timestamp}] Total Published Message Count : {_messageCount}");
 
+        // Reset message count if it exceeds int.MaxValue
         if (_messageCount >= int.MaxValue) { Interlocked.Exchange(ref _messageCount, 0); }
     }
 
--- ETLSimulator/Program.cs
+++ ETLSimulator/Program.cs
@@ -3,34 +3,35 @@ using ETLSimulator.Handler;
 
 Console.WriteLine("ETL, World!");
 
-const string BROKER_ADDRESS = "220.90.135.6";
+const string BROKER_ADDRESS = "220.90.135.22";
 const int PORT = 1883;
 const bool USE_TLS = false;
 
 MqttClientHandler mqttClientHandlerForTotal
-    = new MqttClientHandler(BROKER_ADDRESS, PORT, USE_TLS);
+    = new(BROKER_ADDRESS, PORT, USE_TLS);
 IntegrationController totalDataController
-    = new IntegrationController(mqttClientHandlerForTotal.Client);
+    = new(mqttClientHandlerForTotal.Client);
 
-const int MILLI_SECONDS = 1_000;
+const int MILLI_SECONDS = 100;
 const int MAX_COUNT = 1;
 
 _ = Task.Run(async () =>
 {
     await totalDataController.SendAGVLoop(
        topic: "XR/data/4e7a17a46bbf09a94af971efe37a8340/AGV",
-       jobId: "4e7a17a46bbf09a94af971efe37a8340/AGV",
        milliseconds: MILLI_SECONDS,
        maxCount: MAX_COUNT);
 });
+
+const int TRANSPORT_MILLI_SECONDS = 1_000;
+const int TRANSPORT_MAX_COUNT = 1;
 
 _ = Task.Run(async () =>
 {
     await totalDataController.SendTransportLoop(
        topic: "XR/data/b0fce4b111ab01ab148c94de81120404/TRANSPORT_JOB",
-       jobId: "b0fce4b111ab01ab148c94de81120404/TRANSPORT_JOB",
-       milliseconds: MILLI_SECONDS,
-       maxCount: MAX_COUNT);
+       milliseconds: TRANSPORT_MILLI_SECONDS,
+       maxCount: TRANSPORT_MAX_COUNT);
 });
 
 // Set Exit Point
